81bf75b0c11300a44dbafdcfa508f728031b198a
Abdullahi <hakeemabdullah87@gmail.com>
2024-05-08 22:26:28 +0000

    FEAT: add script file 0-basic_cache.py for task 0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Creating a Basic Cache Class inheriting from BaseCaching and is a
+caching system
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Creating a Cache Class"""
+
+    def __init__(self):
+        """Class Initialization Dunder Method"""
+        super().__init__()
+
+    def put(self, key, item):
+        """Method for putting items in a dictionary"""
+        if key is None and item is None:
+            pass
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Method that returns the value in self.cache_data"""
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data.get(key)
