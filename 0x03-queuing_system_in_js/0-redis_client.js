7a723b78327dfae689009aa8aeb0434f5595d594
Abdullahi <hakeemabdullah87@gmail.com>
2024-06-12 17:16:37 +0000

    FEAT: create all files for task 0

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,7 @@
+import { createClient } from 'redis'
+const client = createClient();
+
+(async () => {
+    client.on('error', (err) => console.log("Redis client not connected to server:", err));
+    client.on('connect', () => console.log("Redis Client connected to the server"));
+})();
